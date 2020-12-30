0d3d7a32efc696b0871804e5a795edd52ff6f794
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-12-30 18:29:12 -0300

    [project-01] Adds basic element to the layout

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import {SafeAreaView, View, StyleSheet, Text, StatusBar} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Dimensions,
+  FlatList,
+  Animated,
+  Image,
+} from 'react-native';
 
 const images = {
   man:
@@ -18,26 +27,60 @@ const data = Object.keys(images).map((i) => ({
   title: i,
   image: images[i],
 }));
+const {width, height} = Dimensions.get('screen');
+
+const Indicator = () => {
+  return <View style={styles.indicator} />;
+};
+
+const Tab = ({item}) => {
+  return (
+    <View>
+      <Text style={[styles.tabItem, {fontSize: 84 / data.length}]}>
+        {item.title}
+      </Text>
+    </View>
+  );
+};
+
+const Tabs = ({scrollX}) => {
+  return (
+    <View style={styles.tabs}>
+      <View style={styles.tabsBox}>
+        {data.map((item) => {
+          return <Tab key={item.key} item={item} />;
+        })}
+      </View>
+      <Indicator />
+    </View>
+  );
+};
 
 const App = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <View style={styles.container}>
-        <StatusBar hidden />
-        <Text style={{fontSize: 42}}>❤️</Text>
-        <Text
-          style={{
-            fontFamily: 'Menlo',
-            marginTop: 10,
-            fontWeight: '800',
-            fontSize: 16,
-          }}>
-          Expo
-        </Text>
-        <Text style={{fontFamily: 'Menlo', fontStyle: 'italic', fontSize: 12}}>
-          (expo.io)
-        </Text>
+        <Animated.FlatList
+          data={data}
+          keyExtractor={(item) => item.key}
+          horizontal
+          showsVerticalScrollIndicator={false}
+          pagingEnabled
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {x: scrollX}}}],
+            {useNativeDriver: false}, // native driver won't works with width resize
+          )}
+          bounces={false}
+          renderItem={(item) => (
+            <View style={styles.imageWrapper}>
+              <Image source={{uri: item.image}} style={styles.image} />
+              <View style={styles.imageMask} />
+            </View>
+          )}
+        />
+        <Tabs scrollX={scrollX} />
       </View>
     </>
   );
@@ -49,6 +92,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageWrapper: {
+    width: width,
+    height: height,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  imageMask: {
+    width: width,
+    height: height,
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  tabs: {
+    position: 'absolute',
+    top: 100,
+    width: width,
+  },
+  tabsBox: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  tabItem: {
+    fontWeight: '800',
+    color: 'white',
+    textTransform: 'uppercase',
+  },
+  indicator: {
+    position: 'absolute',
+    height: 4,
+    width: 100,
+    backgroundColor: 'white',
+    bottom: -10,
   },
 });
 
