cadf79952b013990dba8c0161406c1a5d735f131
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-01-03 20:52:10 -0300

    [project-02] Adds starter code

--- App.js
+++ App.js
@@ -1,14 +1,69 @@
 import React from 'react';
-import {View, StyleSheet, Text, StatusBar} from 'react-native';
+import {
+  StatusBar,
+  Animated,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+const {width, height} = Dimensions.get('screen');
+
+// https://www.flaticon.com/packs/retro-wave
+// inspiration: https://dribbble.com/shots/11164698-Onboarding-screens-animation
+// https://twitter.com/mironcatalin/status/1321180191935373312
+
+const bgs = ['#A5BBFF', '#DDBEFE', '#FF63ED', '#B98EFF'];
+const DATA = [
+  {
+    key: '3571572',
+    title: 'Multi-lateral intermediate moratorium',
+    description:
+      "I'll back up the multi-byte XSS matrix, that should feed the SCSI application!",
+    image: 'https://image.flaticon.com/icons/png/256/3571/3571572.png',
+  },
+  {
+    key: '3571747',
+    title: 'Automated radical data-warehouse',
+    description:
+      'Use the optical SAS system, then you can navigate the auxiliary alarm!',
+    image: 'https://image.flaticon.com/icons/png/256/3571/3571747.png',
+  },
+  {
+    key: '3571680',
+    title: 'Inverse attitude-oriented system engine',
+    description:
+      'The ADP array is down, compress the online sensor so we can input the HTTP panel!',
+    image: 'https://image.flaticon.com/icons/png/256/3571/3571680.png',
+  },
+  {
+    key: '3571603',
+    title: 'Monitored global data-warehouse',
+    description: 'We need to program the open-source IB interface!',
+    image: 'https://image.flaticon.com/icons/png/256/3571/3571603.png',
+  },
+];
 
 const App = () => {
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <Text>Project 02 Start</Text>
-      </View>
-    </>
+      <Text style={{fontSize: 42}}>❤️</Text>
+      <Text
+        style={{
+          fontFamily: 'Menlo',
+          marginTop: 10,
+          fontWeight: '800',
+          fontSize: 16,
+        }}>
+        Expo
+      </Text>
+      <Text style={{fontFamily: 'Menlo', fontStyle: 'italic', fontSize: 12}}>
+        (expo.io)
+      </Text>
+    </View>
   );
 };
 
